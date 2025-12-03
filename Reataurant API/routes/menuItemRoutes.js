38257373da0863a33a2028e5a0098bb53b199515
DevShami <162372274+DevShami1@users.noreply.github.com>
2025-12-03 18:34:29 +0300

    Delete Reataurant API directory

--- Reataurant API/routes/menuItemRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const menuItemController = require('../controllers/menuItemController')
-
-router.get('/' , menuItemController.getAllMenuItems)
-router.get('/:categoryID' , menuItemController.getMenuItemsByCategoryID)
-
-module.exports = router
